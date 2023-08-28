fd0274cf150481ec5cf7576c408db6ef034e8d21
Gabriel Ramos <gabriel.ramos@deptagency.com>
2023-08-28 18:28:59 +0200

    feat: :sparkles: Added app navigation

--- App.tsx
+++ App.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import {NativeBaseProvider} from 'native-base';
 import {theme} from './src/theme';
+import {persistor, store} from './src/Redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
+import {Provider} from 'react-redux';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Home} from './src/screens/home.screen';
+
+const Stack = createNativeStackNavigator();
+
+const Navigation = () => (
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
 
 const App = () => {
-  return <NativeBaseProvider theme={theme} />;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <NativeBaseProvider theme={theme}>
+          <Navigation />
+        </NativeBaseProvider>
+      </PersistGate>
+    </Provider>
+  );
 };
 
 export default App;
--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -2,7 +2,7 @@ import {applyMiddleware, legacy_createStore as createStore, Store} from 'redux';
 import thunk from 'redux-thunk';
 import {appReducer} from './reducer';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {persistReducer} from 'redux-persist';
+import {persistReducer, persistStore} from 'redux-persist';
 
 const persistSettings = {
   key: 'root',
@@ -13,8 +13,9 @@ const persistSettings = {
 const reducer = persistReducer(persistSettings, appReducer);
 
 const store: Store<AppState> = createStore(reducer, applyMiddleware(thunk));
+const persistor = persistStore(store);
 
 export type AppState = ReturnType<typeof reducer>;
 export type AppDispatch = typeof store.dispatch;
 
-export {store};
+export {store, persistor};
