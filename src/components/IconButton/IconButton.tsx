16c55e1134f6f3bafe443fb22e24d5bdf26fa40c
Gabriel Ramos <gabriel.ramos@deptagency.com>
2023-08-28 23:13:16 +0200

    feat: :label: IconButton color prop

--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -4,13 +4,19 @@ import MaterialIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 interface IconButtonProps {
   icon: string;
+  iconColor?: string;
   onPress: () => void;
 }
 
-export const IconButton: React.FC<IconButtonProps> = ({icon, onPress}) => {
+export const IconButton: React.FC<IconButtonProps> = ({
+  icon,
+  iconColor,
+  onPress,
+}) => {
   return (
     <Pressable
       onPress={onPress}
+      color={iconColor}
       _pressed={{style: {transform: [{scale: 0.9}]}}}>
       <Icon size="5" as={<MaterialIcons name={icon} />} />
     </Pressable>
