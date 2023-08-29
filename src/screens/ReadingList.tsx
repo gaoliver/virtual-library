8083dc8dd10512365ae7ad7f08c195737705e26e
Gabriel Ramos <gabriel.ramos@deptagency.com>
2023-08-29 14:20:50 +0200

    feat: :lipstick: Reading List screen styled

--- src/screens/ReadingList.tsx
+++ src/screens/ReadingList.tsx
@@ -1,10 +1,50 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {RootBottomParamList} from 'App';
+import {Box, Center, FlatList, Text} from 'native-base';
+import {BookCard, Header} from '@/components';
+import {spaces} from '@/constants/spaces';
+import {BookProps} from '@/@types/models';
+import {useSelector} from 'react-redux';
+import {AppState} from '@/Redux/slices';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
-export const ReadingList = () => {
+type NavigationProps = NativeStackNavigationProp<
+  RootBottomParamList,
+  'Favourites'
+>;
+
+export const ReadingList = ({navigation}: {navigation: NavigationProps}) => {
+  const readingList = useSelector((state: AppState) => state.readingList);
+
+  const handlePressCard = (book: BookProps) => {
+    navigation.navigate('SearchNavigator', {
+      screen: 'BookDetails',
+      params: {book},
+    });
+  };
+
   return (
-    <View>
-      <Text>ReadingList</Text>
-    </View>
+    <Box flex={1}>
+      <Header title="Reading List" />
+
+      {!readingList.length ? (
+        <Center pt={4}>
+          <Text>No book saved yet.</Text>
+        </Center>
+      ) : (
+        <FlatList
+          pt={'1'}
+          px={spaces.screenWidth}
+          data={readingList}
+          renderItem={({item}) => (
+            <BookCard
+              my={'1'}
+              book={item}
+              onPress={() => handlePressCard(item)}
+            />
+          )}
+        />
+      )}
+    </Box>
   );
 };
